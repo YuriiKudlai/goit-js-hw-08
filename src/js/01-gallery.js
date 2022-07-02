16c83ec3ec9563fdf45f93d70f795fc533f808ad
YuriiKudlai <kudlai.yurii@gmail.com>
2022-07-02 17:25:34 +0300

    create task-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,6 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-// Описан в документации
 import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 
 
 const galleryContainer = document.querySelector('.gallery');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(toFindSecondsPlay, 1000));
+
+function toFindSecondsPlay(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
