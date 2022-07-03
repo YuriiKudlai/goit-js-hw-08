5f0d0f4646cc47384f3c84452c505c4b05106cb2
YuriiKudlai <kudlai.yurii@gmail.com>
2022-07-03 15:01:54 +0300

    update task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const form = document.querySelector('.feedback-form');
+populateTextarea();
+
+form.addEventListener('input', throttle(onFieldsInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFieldsInput() {
+  const createObjForm = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(createObjForm));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  if (savedMessage) {
+    const savedDataForm = JSON.parse(savedMessage);
+    email.value = savedDataForm.email;
+    message.value = savedDataForm.message;
+  }
+}
+
 
